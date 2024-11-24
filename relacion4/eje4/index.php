736efccde0b84dd85ca63a777d7a9cf9cbda9fb6
Virtual-Truck <juangarros2005@gmail.com>
2024-11-24 18:58:16 +0100

    correccion

--- relacion4/eje4/index.php
+++ relacion4/eje4/index.php
@@ -14,26 +14,6 @@
         <input type="submit" value="Enviar">
     </form>
     <?php
-    /*Se pretende obtener información de un fichero a partir de su nombre. Para ello se van
-        a definir las siguientes funciones:
-        a) string calcula_extension (string)
-        Recibe una cadena de caracteres que representa el nombre de un fichero y
-        devuelve una cadena con la extensión del fichero.
-        Ejemplo: la llamada
-        $extension = calcula_extension (“curriculum.pdf”);
-        devuelve en la variable $extension el valor “PDF”.
-
-        b) string tipo_fichero (string)
-        Recibe una cadena de caracteres que representa una extensión de fichero y
-        devuelve una cadena con el tipo de fichero que corresponde a dicha extensión
-        (véase Tabla I).
-        
-        Ejemplo: $tipo = tipo_fichero (“PDF”);
-        devuelve en la variable $tipo el valor “Documento Adobe PDF”.
-        
-        Se pide: Crear un fichero cadena.php con estas funciones y una página practica4a.php que,
-        utilizando el código que se proporciona, obtenga y muestre el tipo de un fichero a partir de
-        una variable con su nombre (figura 1).*/
     include(dirname(__FILE__) . "/" . "practica4/" . "cadena.php");
 
     $tipoArchivos = [
--- relacion4/eje4/practica4/cadena.php
+++ relacion4/eje4/practica4/cadena.php
@@ -1,20 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-      function calculamosExtension(&$fichero){
-        if(!empty($fichero)){
-            $fichero=strstr($_POST['cadena'],'.');
-            echo "<br> <h3>La extension del fichero es : $fichero</h3>";
-        }
-      }
 
-      /*function tipo_fichero ($fichero){
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  /*Se pretende obtener información de un fichero a partir de su nombre. Para ello se van
+        a definir las siguientes funciones:
+        
+        a) string calcula_extension (string)
+        Recibe una cadena de caracteres que representa el nombre de un fichero y
+        devuelve una cadena con la extensión del fichero.
+        Ejemplo: la llamada
+        $extension = calcula_extension (“curriculum.pdf”);
+        devuelve en la variable $extension el valor “PDF”.
+
+        b) string tipo_fichero (string)
+        Recibe una cadena de caracteres que representa una extensión de fichero y
+        devuelve una cadena con el tipo de fichero que corresponde a dicha extensión
+        (véase Tabla I).
+
+        Ejemplo: $tipo = tipo_fichero (“PDF”);
+        devuelve en la variable $tipo el valor “Documento Adobe PDF”.
+        
+    */
+
+
+  function calculamosExtension(&$fichero)
+  {
+    if (!empty($fichero)) {
+      $fichero = strstr($_POST['cadena'], '.');
+      echo "<br> <h3>La extension del fichero es : $fichero</h3>";
+    }
+  }
+
+  /*function tipo_fichero ($fichero){
       switch($fichero){
           case "PDF":
             return "Documento Adobe PDF";
@@ -46,15 +70,17 @@
         }
       }*/
 
-      function devuelveTipo(&$fichero){
-        if(!empty($fichero)){
-          $fichero= ltrim($fichero,'.');
-          $fichero=strtoupper($fichero);
-        }
-      }
+  function devuelveTipo(&$fichero)
+  {
+    if (!empty($fichero)) {
+      $fichero = ltrim($fichero, '.');
+      $fichero = strtoupper($fichero);
+    }
+  }
 
 
 
-    ?>
+  ?>
 </body>
+
 </html>
