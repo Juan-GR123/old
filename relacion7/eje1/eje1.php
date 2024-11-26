5a99f527c4af58b59bbec72298990704a52c0729
Virtual-Truck <juangarros2005@gmail.com>
2024-11-26 20:10:09 +0100

    relacion7

---
+++ relacion7/eje1/eje1.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    /*Realiza las siguientes actividades:
+
+a.      Crea la clase Coche.  Ten en cuenta que de un coche sabemos color,
+        marca, modelo, velocidad, caballos y nº de plazas.
+
+b.      Las propiedades tendrán los siguientes valores por defecto:
+
+c.      Implementa un método que permita frenar y otro acelerar ( suma y resta uno a la velocidad)
+
+d.      Guarda la clase en un archivo llamado coche.php
+
+e.      A partir de esta clase crea los objetos que necesites y el script que nos
+        permita mostrar lo siguiente:*/
+
+    class Coche
+    {
+        public function __construct(
+            private string $color = "Rojo",
+            private string $marca = "Ferrari",
+            private int $velocidad = 300,
+            private string $modelo = "Aventador",
+            private int $caballos = 500,
+            private int $n_plazas = 2
+        ) {}
+        public function acelerar()
+        {
+            $this->velocidad += 1;
+        }
+
+        public function frenar()
+        {
+            $this->velocidad -= 1;
+        }
+
+        // Métodos getter
+
+
+
+
+        /**
+         * Get the value of color
+         */
+        public function getColor()
+        {
+            return $this->color;
+        }
+
+        /**
+         * Set the value of color
+         *
+         * @return  self
+         */
+        public function setColor($color)
+        {
+            $this->color = $color;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of marca
+         */
+        public function getMarca()
+        {
+            return $this->marca;
+        }
+
+        /**
+         * Set the value of marca
+         *
+         * @return  self
+         */
+        public function setMarca($marca)
+        {
+            $this->marca = $marca;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of velocidad
+         */
+        public function getVelocidad()
+        {
+            return $this->velocidad;
+        }
+
+        /**
+         * Set the value of velocidad
+         *
+         * @return  self
+         */
+        public function setVelocidad($velocidad)
+        {
+            $this->velocidad = $velocidad;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of modelo
+         */
+        public function getModelo()
+        {
+            return $this->modelo;
+        }
+
+        /**
+         * Set the value of modelo
+         *
+         * @return  self
+         */
+        public function setModelo($modelo)
+        {
+            $this->modelo = $modelo;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of caballos
+         */
+        public function getCaballos()
+        {
+            return $this->caballos;
+        }
+
+        /**
+         * Set the value of caballos
+         *
+         * @return  self
+         */
+        public function setCaballos($caballos)
+        {
+            $this->caballos = $caballos;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of n_plazas
+         */
+        public function getN_plazas()
+        {
+            return $this->n_plazas;
+        }
+
+        /**
+         * Set the value of n_plazas
+         *
+         * @return  self
+         */
+        public function setN_plazas($n_plazas)
+        {
+            $this->n_plazas = $n_plazas;
+
+            return $this;
+        }
+    }
+
+    ?>
+</body>
+
+</html>
--- relacion7/ejemplos/empleados.php
+++ relacion7/ejemplos/empleados.php
@@ -1,35 +1,52 @@
-<?php 
+<?php
 
-class Empleado{
+class Empleado
+{
     public $Nombre;
     public $Apellidos;
 
-    public function setNombre($nombre){
-        $this->Nombre=$nombre;
+    public function __construct($nombre=null, $apellidos=null)
+    {
+        $this->Nombre = $nombre;
+        $this->Apellidos = $apellidos;
+    }
+    public function setNombre($nombre)
+    {
+        $this->Nombre = $nombre;
     }
 
-    public function getNombre(){
+    public function getNombre()
+    {
         return $this->Nombre;
     }
 
-    public function setApellidos($apellidos){
-        $this->Apellidos=$apellidos;
+    public function setApellidos($apellidos)
+    {
+        $this->Apellidos = $apellidos;
     }
 
-    public function getApellidos(){
+    public function getApellidos()
+    {
         return $this->Apellidos;
     }
 
-    public function __toString(){
-        return "<br> - Empleado: " . $this->Nombre . " - " .$this->Apellidos . "<br>";
+    public function __toString()
+    {
+        return "<br> - Empleado: " . $this->Nombre . "  " . $this->Apellidos . "<br>";
     }
-
 }
+//require_once: solo llama una vez a las funciones para no redefinir clases 
+//include: llama a las funciones todas las veces que es llamada
 
-$emp=new Empleado;
+$emp = new Empleado();
 
-$emp ->setNombre("Juan");
-$emp ->setApellidos("Alvarez Manzano");
+$emp->setNombre("Juan");
+$emp->setApellidos("Alvarez Manzano");
 echo $emp;
+
+
+$emp2 = new Empleado("Pedro", "Paramo");
+echo $emp2;
+
 
 ?>
