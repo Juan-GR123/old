4ff760f4aa85168c2369ed4315292527cde6d487
Virtual-Truck <juangarros2005@gmail.com>
2025-01-12 23:10:47 +0100

    tabulado

--- relacion8/empresa/empresa.php
+++ relacion8/empresa/empresa.php
@@ -42,40 +42,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register']) && $_SESSI
     }
 }
 
-    //iniciar sesion
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login']) && $_SESSION['errorInicioSesion'] < 3) {
-        // Compruebo que el email es válido
-        $email = filter_var(trim($_POST['email_login']), FILTER_VALIDATE_EMAIL);
-        // Quito los espacios en blanco al comienzo y final de la contraseña
-        $password = trim($_POST['password_login']);
+//iniciar sesion
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login']) && $_SESSION['errorInicioSesion'] < 3) {
+    // Compruebo que el email es válido
+    $email = filter_var(trim($_POST['email_login']), FILTER_VALIDATE_EMAIL);
+    // Quito los espacios en blanco al comienzo y final de la contraseña
+    $password = trim($_POST['password_login']);
 
-        if ($email && $password) {
-            $sql = "SELECT id FROM usuarios WHERE email = :email";
+    if ($email && $password) {
+        $sql = "SELECT id FROM usuarios WHERE email = :email";
 
-            $stmt = $pdo->prepare($sql); //Preparamos la consulta para que se realice en modo seguro
-            $stmt->bindParam(':email', $email);
-            $stmt->execute();
+        $stmt = $pdo->prepare($sql); //Preparamos la consulta para que se realice en modo seguro
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
 
-            if ($stmt->rowCount() == 1) {//rowCount te dice si la consulta devuelve algo
-                $user = $stmt->fetch();
-                $verify=password_verify($$password,$user['password_hash']);
-                if($verify){
-                    $_SESSION['errorInicioSesion'] = 0;
-                    $_SESSION['rol'] = $user['rol'];
-                    $_SESSION['nombre'] = $user['email'];
-                    header('Location: index.php');
-                    exit();
-                }
-            }else{
-                $_SESSION['errorInicioSesion']++;
-                $_SESSION['ultimoIntento'] = time(); // Registro la hora del último intento fallido
+        if ($stmt->rowCount() == 1) { //rowCount te dice si la consulta devuelve algo
+            $user = $stmt->fetch();
+            $verify = password_verify($$password, $user['password_hash']);
+            if ($verify) {
+                $_SESSION['errorInicioSesion'] = 0;
+                $_SESSION['rol'] = $user['rol'];
+                $_SESSION['nombre'] = $user['email'];
+                header('Location: index.php');
+                exit();
             }
         } else {
-            echo "Email no registrado.";
+            $_SESSION['errorInicioSesion']++;
+            $_SESSION['ultimoIntento'] = time(); // Registro la hora del último intento fallido
         }
-    }else{
-        echo "Por favor, vuelve a introducir las credenciales";
+    } else {
+        echo "Email no registrado.";
     }
+} else {
+    echo "Por favor, vuelve a introducir las credenciales";
+}
 
 
 
@@ -94,18 +94,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register']) && $_SESSI
 </head>
 
 <body>
-<h2>Registros</h2>
-<form method="POST">
-    <label for="email">Email:</label>
-    <input type="email" name="email_register">
-    <label for="password_register"> Contraseña: </label>
-    <input type="password" name="password_register">
-    <input type="submit" name="register" value="Registrar">
-</form>
+    <h2>Registros</h2>
+    <form method="POST">
+        <label for="email">Email:</label>
+        <input type="email" name="email_register">
+        <label for="password_register"> Contraseña: </label>
+        <input type="password" name="password_register">
+        <input type="submit" name="register" value="Registrar">
+    </form>
 
 
-  <?php if ($_SESSION['errorInicioSesion'] < 3) { ?>
-        <h1>Iniciar Sesion: 1</h1>
+    <?php if ($_SESSION['errorInicioSesion'] < 3) { ?>
+        <h1>Iniciar Sesion:</h1>
         <form method="POST">
             <label for="email_login">Email:</label>
             <input type="text" id="email_login" name="email_login"><br><br>
@@ -113,13 +113,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register']) && $_SESSI
             <label for="password_login">Contraseña:</label>
             <input type="password" id="password_login" name="password_login"><br><br>
             <input type="submit" name="login" value="Iniciar Sesion">
-            
         </form>
     <?php
     } else {
-     echo "<h2>Has superado los intentos permitidos</h2>";
+        echo "<h2>Has superado los intentos permitidos</h2>";
     }
-    
+
     ?>
 </body>
 
