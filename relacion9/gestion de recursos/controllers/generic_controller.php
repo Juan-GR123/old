1b538020f004f5dd48a7dff49e1adcc5473d2a8f
Juan-GR123 <juangarros2005@gmail.com>
2025-02-07 09:13:13 +0100

    reservas

---
+++ relacion9/gestion de recursos/controllers/generic_controller.php
@@ -0,0 +1,60 @@
+<?php
+
+// CONTROLADOR DE ...
+include_once("models/generic_model.php");  // Modelos
+include_once("view.php");              // Plantilla de vistas
+
+class GenericController
+{
+    private $db;             // Conexión con la base de datos
+    private $model_name;     // Modelo
+    private $model;          
+
+    public function __construct()
+    {
+        $this->model = new GenericModel();
+    }
+
+    // --------------------------------- MOSTRAR TODOS ----------------------------------------
+    public function show()
+    {
+    }
+
+    // --------------------------------- FORMULARIO INSERCIÓN ----------------------------------------
+
+    public function formInsert()
+    {
+    }
+
+    // --------------------------------- INSERCIÓN ----------------------------------------
+
+    public function insert()
+    {
+    }
+
+    // --------------------------------- BORRADO ----------------------------------------
+
+    public function delete()
+    {
+    }
+
+    // --------------------------------- FORMULARIO ACTUALIZACIÓN ----------------------------------------
+
+    public function formUpdate()
+    {
+    }
+
+    // --------------------------------- MODIFICACIÓN ----------------------------------------
+
+    public function update()
+    {
+    }
+
+    // --------------------------------- BÚSQUEDA ----------------------------------------
+
+    public function search()
+    {
+    }
+
+
+} // class
